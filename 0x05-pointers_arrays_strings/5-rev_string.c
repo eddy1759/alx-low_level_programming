ac74ce76934d372a3ebde13d07406f873c268e9a
eddy1759 <eddy1759@gmail.com>
2022-03-21 15:50:07 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *rev_string - prints reversed string, followed by a new line
+ *@s: pointer to the string to print
+ *Return: void
+*/
+void rev_string(char *s)
+{
+	int len, i;
+	char l;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	len--;
+	for (i = 0; i < len; i++)
+	{
+		l = *(s + len);
+		*(s + len) = *(s + i);
+		*(s + i) = l;
+		len--;
+	}
+}
